48593edb131f6c2c177d9c5b7745f6acc91098e3
Joe Waddington <38754787+JoeWaddington@users.noreply.github.com>
2021-01-11 15:14:08 +0000

    RabbitMQ Testing
    Testing rabbitmq messaging system

---
+++ StockTrackingApp/Receive/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Building the connection factory, contains default username, password and localhost
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "stockqueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+
+                // Method to consume and handle messages
+                consumer.Received += (model, ea) =>
+                  {
+                      var body = ea.Body.ToArray();
+                      var message = Encoding.UTF8.GetString(body);
+                      Console.WriteLine("Received: {0}", message);
+                  };
+                channel.BasicConsume("stockqueue", autoAck:true, consumer:consumer);
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }  
+        }
+    }
+}
---
+++ StockTrackingApp/Send/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Building the connection factory, contains default username, password and localhost
+            var factory = new ConnectionFactory() 
+            { 
+                UserName = "guest",
+                Password= "guest",
+                HostName = "localhost" 
+            };
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue:"stockqueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                string message = "hello";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange:"", routingKey:"stockqueue", basicProperties:null, body:body);
+                Console.WriteLine("Sent {0}", message);
+            }
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
